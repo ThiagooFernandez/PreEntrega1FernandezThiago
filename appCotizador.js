02aadc3ef29a054f4ed52258482cd3d5ea9b8050
ThiagooFernandez <114247536+ThiagooFernandez@users.noreply.github.com>
2022-12-05 15:51:23 -0300

    Add files via upload

--- appCotizador.js
+++ appCotizador.js
@@ -1,70 +1,47 @@
+var mensaje = document.querySelector("#mensaje");
+mensaje.style.display = 'none';
 const cotizarSeguro=()=>{
-    let marca=document.querySelector("#marca").value;
+    let marca=document.getElementById("marca").value;
     let year=document.querySelector("#year").value;
-    let basico=document.querySelector("#basico").value;
-    let completo=document.querySelector("#completo").value;
+    let basico=document.getElementById("plan").value;
 
     let divResumen=document.querySelector("#resumen");
     let divResultado=document.querySelector("#resultado");
     divResultado.style.display="none"
 
-    let plan="";
-
-    if(basico.checked){
-        plan="basico"
-    }else if(completo.checked){
-        plan="completo"
-    }
-
-    
-
     if(marca ==='' || 
-        year==='' || 
-        plan===''){
-        mostrarError("#msj-error","FALTA SELECCIONAR LAS OPCIONES");
+        year==='' || basico === ''){
+        mostrarError("msj-error","FALTA SELECCIONAR LAS OPCIONES");
         return;
+        }else{
+            let cotizacion={marca,year, basico};
+            mensaje.style.display='block';
+    
+    
+            divResumen.style.backgroundColor="#FFF";
+            divResumen.style.display="block";
+    
+            divResumen.innerHTML=`<div style="text-aling-center">
+                                    <img src="cargando.gif" width=300 height=300>
+                                  </div>`;
+            
+            setTimeout(() => {
+                divResumen.style.backgroundColor="#00838F";
+                divResumen.innerHTML=`
+                <h2>Resumen de Cotizacion</h2>
+                <ul>
+                    <li>Marca: ${mayuscula(marca)}</li>
+                    <li>Año del auto: ${mayuscula(year)}</li>
+                    <li>Plan:  ${basico}</li> 
+                    <li>Total:  ${parseInt(marca) + parseInt(year) + parseInt(basico)}</li> 
+                      
+                </ul>`
+    
+            }, 4000);
         }
-        let cotizacion={marca,year, plan};
-        document.querySelector("#mensaje").style.display="none";
-
-
-        divResumen.style.backgroundColor="#FFF";
-        divResumen.style.display="block";
-
-        divResumen.innerHTML=`<div style="text-aling-center">
-                                <img src="cargando.gif" width=300 height=300>
-                              </div>`;
-        
-        setTimeout(() => {
-            divResumen.style.backgroundColor="#00838F";
-            divResumen.innerHTML=`
-            <h2>Resumen de Cotizacion</h2>
-            <ul>
-                <li>Marca: ${mayuscula(marca)}</li>
-                <li>Marca: ${mayuscula(marca)}</li>
-                <li>Plan:  ${plan}</li> 
-                <li>Año del auto:  ${year}</li> 
-                  
-            </ul>`
-
-            let cotizacionFinal=cotizar(cotizacion);
-        divResultado.style.display="block";
-        divResultado.className="divResultado";
-        divResultado.innerHTML=` <p class="textoCotizacion">$${cotizacionFinal}</p>`;
-        }, 3000);
         
     }
-const cotizar=(cotizacion)=>{
-    const{marca, year, plan}=cotizacion;
-    let resultado=2000;
-    const diferenciaYear=diferencia(year)
-    resultado-=((diferenciaYear*3)*resultado)/100;
 
-    resultado=calcularMarca(marca)*resultado;
-    const incrementoPlan=obtenerPlan(plan);
-    resultado=parseFloat(incrementoPlan*resultado).toFixed(2);
-    return resultado;
-}
 const obtenerPlan=plan=>{
     return (plan ==='basico')?1.20:1.50;
 }
@@ -89,10 +66,13 @@ const mayuscula=(palabra)=>{
 }
 
 const mostrarError=(elemento,mensaje)=>{
-    divError=document.querySelector(elemento);
-    divError.innerHTML=`<p class="alerta-error-danger error">${mensaje}</p>`;
+    divError=document.getElementById(elemento);
+    console.log("soy div",divError)
+    divError.innerHTML="<p class='alerta-error-danger error' id='borrar'>FALTA SELECCIONAR LAS OPCIONES</p>";
+    eliminado=document.getElementById('borrar');
+
     setTimeout(() => {
-        divError.innerHTML==``;
+        divError.removeChild(eliminado);
     }, 2000);
 }
 
